68e15e77fc877220977e8398bef6f82c7aa36e6b
Clausemberg Rodrigues de Oliveira <clausemberg@yahoo.com.br>
2026-01-04 09:09:50 -0300

    Add tag model

---
+++ tag.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+from pymongo import ObjectId
+
+class Tag(BaseModel):
+    id = Field(default_factory=lambda: str(ObjectId()), alias="_id")
+    descricao: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        allow_population_by_field_name = True
