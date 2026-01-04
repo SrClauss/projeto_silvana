e5de35fa6da9703a704b1683976bdd8dcaf343a5
Clausemberg Rodrigues de Oliveira <clausemberg@yahoo.com.br>
2026-01-04 10:23:17 -0300

    Update Item model in itens.py

--- fastapi/api/models/itens.py
+++ fastapi/api/models/itens.py
@@ -1,15 +1,6 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional
+from pydantic import BaseModel
 from datetime import datetime
-from pymongo import ObjectId
 
 class Item(BaseModel):
-    id = Field(default_factory=lambda: str(ObjectId()), alias="_id")
-    description: Optional[str] = None
-    price: float
     quantity: int
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: Optional[datetime] = None
-
-    class Config:
-        allow_population_by_field_name = True
+    acquisition_date: datetime
